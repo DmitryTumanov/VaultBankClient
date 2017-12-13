21c45827d641932236f5a5800bf6f73ca3d94baa
Unknown <tumanov.97.dima@gmail.com>
2017-12-13 03:06:23 +0300

    Fix bug with incorrect main window width

--- app/components/app/app.component.ts
+++ app/components/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {StorageService} from "../../services/storage/storage.service";
 import {SettingsProvider} from "../../providers/settings/settings.provider";
 import {PreLoaderProvider} from "../../providers/preloader/pre-loader.provider";
@@ -7,8 +7,10 @@ import {PreLoaderProvider} from "../../providers/preloader/pre-loader.provider";
     selector: 'my-app',
     templateUrl: './app.component.html'
 })
-export class AppComponent {
-    public isDataPreLoading: boolean;
+export class AppComponent implements OnInit{
+    public mainWindowClass: string = "";
+
+    private isDataPreLoading: boolean;
 
     constructor(private storage: StorageService,
                 private settings: SettingsProvider,
@@ -18,12 +20,32 @@ export class AppComponent {
         });
     }
 
-    isMenuFull(): boolean {
+    ngOnInit(){
+        this.mainWindowClass = this.getMainWindowClass();
+    }
+
+    private getMainWindowClass(): string {
+        let resultClass = "";
+        if (this.isMenuFull()) {
+            resultClass += "full ";
+        }
+        if (this.getMenuStatus() == "block") {
+            resultClass += "block ";
+        } else if (this.getMenuStatus() == "shrink") {
+            resultClass += "shrink ";
+        }
+        if (this.isDataPreLoading) {
+            resultClass += "loaded";
+        }
+        return resultClass;
+    }
+
+    private isMenuFull(): boolean {
         let tokenValue = this.storage.get(this.settings.tokenKey);
         return !tokenValue;
     }
 
-    getMenuStatus(): string {
+    private getMenuStatus(): string {
         return this.storage.get(this.settings.menuStateKey);
     }
 }
